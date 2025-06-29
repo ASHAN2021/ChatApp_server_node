2465f6681d101b8c17e9449a21bff15ff1e9e266
ASHAN2021 <akh8130@gmail.com>
2025-06-29 14:03:26 +0530

    chnages

--- index.js
+++ index.js
@@ -14,14 +14,23 @@ const io = new Server(server, {
 });
 
 app.use(express.json());
+var clients = {};
 
 io.on("connection", (socket) => {
   console.log(`✅ New client connected: ${socket.id}`);
-  socket.on("/test", (msg)=>{
-    console.log(msg);
+  socket.on("signin", (id)=>{
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
     
   })
-
+  socket.on("message", (msg)=>{
+    console.log(msg);
+    let targetId = msg.targetId;
+    if(clients[targetId]){
+      clients[targetId].emit("message",msg);
+    }
+  })
   socket.on("disconnect", () => {
     console.log(`❌ Client disconnected: ${socket.id}`);
   });
